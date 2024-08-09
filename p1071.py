0a53877eb91ebd0802b1cc7eea2ab33b02dbd2c0
p0nyo <boltyourfaceoff@gmail.com>
2024-08-09 14:21:01 +1200

    Created p1071

---
+++ p1071.py
@@ -0,0 +1,11 @@
+def gcdOfStrings(self, str1, str2):
+    """
+    :type str1: str
+    :type str2: str
+    :rtype: str
+    """
+    if str1 + str2 != str2 + str1:
+        return ""
+    
+    
+    
